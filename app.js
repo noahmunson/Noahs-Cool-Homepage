1cf74ffae337b464e5d4da09e6650b9c707c01a5
Noah <no>
2024-02-07 13:00:53 -0600

    Script tag to JS

--- app.js
+++ app.js
@@ -7,3 +7,81 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+function updateClock() {
+  const now = new Date();
+  const dname = now.getDay(),
+    mo = now.getMonth(),
+    dnum = now.getDate(),
+    yr = now.getFullYear(),
+    hou = now.getHours(),
+    min = now.getMinutes(),
+    sec = now.getSeconds(),
+    pe = "AM";
+
+  if (hou >= 12) {
+    pe = "PM";
+  }
+  if (hou == 0) {
+    hou = 12;
+  }
+  if (hou > 12) {
+    hou = hou - 12;
+  }
+
+  Number.prototype.pad = function (digits) {
+    for (var n = this.toString(); n.length < digits; n = 0 + n);
+    return n;
+  };
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Augest",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const ids = [
+    "dayname",
+    "month",
+    "daynum",
+    "year",
+    "hour",
+    "minutes",
+    "seconds",
+    "period",
+  ];
+  const values = [
+    week[dname],
+    months[mo],
+    dnum.pad(2),
+    yr,
+    hou.pad(2),
+    min.pad(2),
+    sec.pad(2),
+    pe,
+  ];
+  for (let i = 0; i < ids.length; i++)
+    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
+
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 10);
+}
+initClock();
